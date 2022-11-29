9dd303d082ed781c406052373cf05b6fe3566a13
Faran Butt <faranbutt789@gmail.com>
2022-11-29 22:34:29 +0500

    Pushing Plane Project

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset=new Vector3(-0.1f,2.97f,-9.8f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+        transform.rotation=plane.transform.rotation;
+    }
+}
